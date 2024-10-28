024840295ca0083c928efe6452d8231f6d0eb67e
Donald2023-source <donalddyusuf@gmail.com>
2024-10-28 14:30:20 -0700

    sidebar

--- app/Dashboard/layout.tsx
+++ app/Dashboard/layout.tsx
@@ -5,8 +5,8 @@ export default function RootLayout({
     children,
 }: { children: React.ReactNode }) {
     return (
-        <div className="flex min-h-screen">
-            <div className="w-1/4 border">
+        <div className="flex min-h-screen gap-2">
+            <div className="w-1/5 border">
                 <Sidebar />
             </div>
             <main className="flex-1">
--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,14 +1,49 @@
-import React from 'react'
+'use client'
+import React from 'react';
+import Image from 'next/image';
+import logo from '@/Assets/logo-white.svg.png';
+import { RiDashboardFill } from "react-icons/ri";
+import { BsHousesFill } from "react-icons/bs";
+import { MdOutlineExplore, MdSettings } from "react-icons/md";
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Sidebar = () => {
-  return (
-    <div>
-      
-      <aside>
-        This is my aside
-      </aside>
-    </div>
-  )
-}
+    const links = [
+        { name: 'Dashboard', icon: <RiDashboardFill />, href: '/Dashboard' },
+        { name: 'Properties', icon: <BsHousesFill />, href: '/Dashboard/properties' },
+        { name: 'Explore', icon: <MdOutlineExplore />, href: '/Dashboard/explore' },
+        { name: 'Settings', icon: <MdSettings />, href: '/Dashboard/settings' },
+    ];
 
-export default Sidebar
+    const pathName = usePathname();
+
+    return (
+        <aside className="w-full">
+            <div className="bg-primary p-3 rounded">
+                <Image src={logo} alt="Logo" />
+            </div>
+
+            <div>
+                {links.map((link) => {
+                    const isActive = pathName === link.href; // Check if link is active
+
+                    return (
+                        <Link
+                            key={link.href}
+                            href={link.href}
+                            className={`flex items-center gap-2 p-3 rounded ${
+                                isActive ? 'bg-secondary text-white' : 'bg-primary text-gray-300'
+                            }`}
+                        >
+                            <h2 className="text-2xl">{link.icon}</h2>
+                            {link.name}
+                        </Link>
+                    );
+                })}
+            </div>
+        </aside>
+    );
+};
+
+export default Sidebar;
